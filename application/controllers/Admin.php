375d966debdcaa26ba14ca6c26b264f633e976cb
vinonada <vinofatikah@gmail.com>
2025-09-12 20:55:37 +0700

    Update fitur X / perbaikan bug Y

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -3,6 +3,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
+	public function __construct(){
+        parent:: __construct();
+        if($this->session->userdata('role') !== 'admin'){
+            redirect('auth');
+        }
+    } 
 	public function index()
 	{
 		$this->template->load('dashboard','home');
